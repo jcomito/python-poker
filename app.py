7eb76e47bb096816b5044b458f99c76f3a61ab07
Jason Comito <jdcomito@protonmail.com>
2026-09-08 22:47:05 -0400

    format w black

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from poker.deck import Deck
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     deck = Deck()
     return render_template("index.html", deck=deck)
-
--- poker/deck.py
+++ poker/deck.py
@@ -1,14 +1,13 @@
 import random
 
 suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
-ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
-         "Jack", "Queen", "King", "Ace"]
+ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
 
 class Deck:
-  def __init__(self):
-    self.cards = []
+    def __init__(self):
+        self.cards = []
 
-    for rank in ranks:
-      for suit in suits:
-        self.cards.append((rank,suit))
-
+        for rank in ranks:
+            for suit in suits:
+                self.cards.append((rank, suit))
